c7a92bb656c56af61bccf09383ed234421c6896e
Nagaraju <nagaraju.kukudala@gmail.com>
2016-11-22 14:39:09 +0530

    Add files via upload

---
+++ app6.3.js
@@ -0,0 +1,20 @@
+//Sum of 1 to 1000 using for loop
+var a=0;
+for(var i =1;i<=1000;i++)
+    {
+        a= a+i;
+        
+    }
+console.log("Sum Of 1 to 1000 numbers using for loop:"+ a);
+
+
+
+//Sum of 1 to 1000 using while loop
+var j=1;
+ sum = 0;
+while(  j<=1000)
+    {
+        sum = sum+j;
+        j++;
+    }
+console.log("Sum Of 1 to 1000 numbers using while loop:"+ sum);
